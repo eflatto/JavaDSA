172dddedfbf66acfe16839c555420e1f5b143ec3
Edwin <Flattoedwin@gmail.com>
2023-11-08 23:21:50 -0500

    codewars challenges

---
+++ CodeWars/src/kyu8/CountingSheep.java
@@ -0,0 +1,10 @@
+package kyu8;
+class CountingSheep {
+    public static String countingSheep(int num) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 1; i <= num; i++) {
+            stringBuilder.append(i).append(" sheep...");
+        }
+        return stringBuilder.toString();
+    }
+}
