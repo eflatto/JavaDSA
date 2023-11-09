c22778fc5af36c5a41e7509ef3d525f553aae72b
Edwin <Flattoedwin@gmail.com>
2023-11-09 00:02:18 -0500

    added another way to calculate the mid point in a binary search

--- SearchingAlgorithms/src/searching/BinarySearch.java
+++ SearchingAlgorithms/src/searching/BinarySearch.java
@@ -11,7 +11,10 @@ public class BinarySearch {
 		int start =0;
 		int end = arr.length -1;
 		
+		
+		
 		while(start <= end) {
+//			int mid = start + (end - start) / 2;
 			int mid = (start + end)/2;
 			
 			if(target<arr[mid]) {
